d5544fc9ff81dcf3462160e98845438a20edbd8a
mepeng <872502023@qq.com>
2020-04-07 17:45:11 +0800

    ssl

---
+++ redis-distribute-lock/src/main/java/com/mepeng/cn/configuration/HttpsConfig.java
@@ -0,0 +1,33 @@
+package com.mepeng.cn.configuration;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置同时支持 HTTP 与 HTTPS 访问
+ */
+//@Configuration
+public class HttpsConfig {
+    @Value("${server.port}")
+    private Integer httpPort;
+    @Bean
+    public ServletWebServerFactory serverFactory() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+    /**
+     * 配置http
+     * @return
+     */
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(httpPort);
+        System.out.println("666666----HttpsConfig httpPort:"+httpPort);
+        return connector;
+    }
+}
