66f1ed5f2cc197c7a8fd7678339b1b004e6426d0
mepeng <872502023@qq.com>
2020-04-09 15:11:46 +0800

    增加配置中心

---
+++ springcloud-config-client/src/main/java/com/mepeng/cn/configClient/controller/ClientController.java
@@ -0,0 +1,18 @@
+package com.mepeng.cn.configClient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClientController {
+    @Value("${dmscloud.sysAuth.name}")
+//    @Value("${word}")
+    private String word;
+
+    @RequestMapping("/hello")
+    public String index(@RequestParam String name) {
+        return name+","+this.word;
+    }
+}
